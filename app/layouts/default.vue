f2f8069fdac809e1e773d670e816fce360fb1952
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2025-04-02 09:13:04 +0200

    chore: migrate fonts to main.css

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -15,12 +15,3 @@ const { isLoading } = storeToRefs(useTeamsStore())
 
 onMounted(store.fetchTeams)
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
-
-:root {
-  font-family: 'Inter', sans-serif;
-  color-scheme: normal;
-}
-</style>
